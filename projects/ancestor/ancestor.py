7c3831857a75a79e442cf6459aa45a5fe7ddaf42
c0derbr1t <c0der.br1t@gmail.com>
2020-10-07 09:17:36 -0500

    Create Queue and Graph classes. Create logic for earliest_ancestor function.
    Passes Tests!

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -16,24 +16,6 @@ class Queue():
         return len(self.queue)
 
 
-class Stack():
-    '''Quick Stack class to assist with Graph class'''
-    def __init__(self):
-        self.stack = []
-
-    def push(self, value):
-        self.stack.append(value)
-
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-
-    def size(self):
-        return len(self.stack)
-
-
 class Graph():
     '''Graph class made for earliest_ancestors function'''
     def __init__(self):
@@ -48,26 +30,6 @@ class Graph():
         else:
             raise IndexError("Nonexistent Vertex")
 
-    def get_neighbors(self, vertex_id):
-        return self.vertices[vertex_id]
-
-    def dft (self, starting_vertex):
-        s = Stack()
-        visited = set()
-        path = []
-
-        s.push(starting_vertex)
-
-        while s.size() > 0:
-            v = s.pop
-            if v not in visited:
-                path.append(v)
-                print(path)
-                visited.add(v)
-
-                for neighbor in self.get_neighbors(v):
-                    s.push(neighbor)
-
 
 def earliest_ancestor(ancestors, starting_node):
     # build graph
@@ -78,9 +40,32 @@ def earliest_ancestor(ancestors, starting_node):
             g.add_vertex(item[0])
         if item[1] not in g.vertices:
             g.add_vertex(item[1])
-        g.add_edge(item[0], item[1])
+        g.add_edge(item[1], item[0])
 
-    g.dft(starting_node)
+    q = Queue()
+
+    max_path_length = 1
+    eldest_ancestor = -1
+
+    q.enqueue([starting_node])
+
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+
+        if len(path) > max_path_length:
+            max_path_length = len(path)
+            eldest_ancestor = v
+        elif len(path) >= max_path_length and v < eldest_ancestor:
+            max_path_length = len(path)
+            eldest_ancestor = v
+
+        for neighbor in g.vertices[v]:
+            new_path = path + [neighbor]
+            q.enqueue(new_path)
+
+    return eldest_ancestor
+
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
